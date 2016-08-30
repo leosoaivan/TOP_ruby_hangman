0eaecb2b13387bb77aaa3521c55a2d706f7b9560
Leonard Soai-Van <leo.soaivan@gmail.com>
2016-08-29 23:22:08 -0400

    Add cleaned dictionary array object

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,9 @@
-dictionary = File.read('5desk.txt')
+def text_correct_size?(word)
+  TRUE if (word.length >= 5 && word.length <= 12)
+end
+
+dictionary = File.readlines('5desk.txt').each { |word| word.gsub!(/\r\n/, "") }
+
+dictionary.delete_if { |word| text_correct_size?(word) != TRUE}
 
 puts dictionary
